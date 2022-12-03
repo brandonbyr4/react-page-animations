61ed775f9b6fd573dce4e03da181ac3836939b2d
ThePlugJumbo <chnxcx@gmail.com>
2022-12-03 11:35:29 -0600

    changed export format

--- package/wrapper.js
+++ package/wrapper.js
@@ -1,19 +1,17 @@
 import { motion } from 'framer-motion';
 
-module.exports = {
-    AnimatedPage: (props) => {
-        const animations = {
-            initial: {opacity: parseInt(props.initialOpacity), x: parseInt(props.initialX)},
-            animate: {opacity: parseInt(props.finalOpacity), x: parseInt(props.finalX)},
-            exit: {opacity: parseInt(props.exitOpacity), x: parseInt(props.exitX)},
-        }
-        return <motion.div
-                    variants={animations}
-                    initial="initial"
-                    animate="animate"
-                    exit="exit"
-                >
-            {props.children}
-        </motion.div>
+module.exports.AnimatedPage = (props) => {
+    const animations = {
+        initial: {opacity: parseInt(props.initialOpacity), x: parseInt(props.initialX)},
+        animate: {opacity: parseInt(props.finalOpacity), x: parseInt(props.finalX)},
+        exit: {opacity: parseInt(props.exitOpacity), x: parseInt(props.exitX)},
     }
+    return <motion.div
+                variants={animations}
+                initial="initial"
+                animate="animate"
+                exit="exit"
+            >
+        {props.children}
+    </motion.div>
 }
