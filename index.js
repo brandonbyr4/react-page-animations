8e13af23ebeffd106c71bb09788a7cf1eebc4bc3
ThePlugJumbo <chnxcx@gmail.com>
2022-12-03 10:08:52 -0600

    adding files

---
+++ index.js
@@ -0,0 +1,19 @@
+const motion = require('framer-motion');
+
+module.exports = {
+    AnimatedPage: (props) => {
+        const animations = {
+            initial: {opacity: parseInt(props.initialOpacity), x: parseInt(props.initialX)},
+            animate: {opacity: parseInt(props.finalOpacity), x: parseInt(props.finalX)},
+            exit: {opacity: parseInt(props.exitOpacity), x: parseInt(props.exitX)},
+        }
+        return <motion.div
+                    variants={animations}
+                    initial="initial"
+                    animate="animate"
+                    exit="exit"
+                >
+            {props.children}
+        </motion.div>
+    }
+}
